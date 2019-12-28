4c9305318a7c2e666b409cdd65942cb6bec7e90e
Keith <keithwipf1@gmail.com>
2019-12-28 15:57:05 -0600

    Fixed all the errors.

--- bass.go
+++ bass.go
@@ -19,9 +19,12 @@ import (
 	"errors"
 )
 type Channel uint64
+func (self Channel) cint() C.ulong {
+	return C.ulong(self)
+}
 
 var(
-	codes=map[int]string {c.BASS_OK: "all is OK", C.BASS_ERROR_MEM:  "memory error", C.BASS_ERROR_FILEOPEN	: "can't open the file", C.BASS_ERROR_DRIVER: "can't find a free/valid driver", c.BASS_ERROR_BUFLOST: "the sample buffer was lost", C.BASS_ERROR_HANDLE: "invalid handle", BASS_ERROR_FORMAT: "unsupported sample format", C.BASS_ERROR_POSITION: "invalid position", C.BASS_ERROR_INIT: "BASS_Init has not been successfully called", C.BASS_ERROR_START: "BASS_Start has not been successfully called", C.BASS_ERROR_SSL: "SSL/HTTPS support isn't available", c.BASS_ERROR_ALREADY: "already initialized/paused/whatever", C.BASS_ERROR_NOTAUDIO: "NOTAUDIO", C.BASS_ERROR_NOCHAN: "can't get a free channel", C.BASS_ERROR_ILLTYPE: "an illegal type was specified", C.BASS_ERROR_ILLPARAM]: "an illegal parameter was specified", C.BASS_ERROR_NO3D: "no 3D support", C.BASS_ERROR_NOEAX: "no EAX support", C.BASS_ERROR_DEVICE: "illegal device number", C.BASS_ERROR_NOPLAY: "not playing", C.BASS_ERROR_FREQ: "illegal sample rate", C.BASS_ERROR_NOTFILE: "the stream is not a file stream", C.BASS_NOHW: "no hardware voices available", C.BASS_ERROR_EMPTY: "the MOD music has no sequence data", C.BASS_ERROR_NONET: "no internet connection could be opened", C.BASS_ERROR_CREATE: "couldn't create the file", C.BASS_ERROR_NOFX: "effects are not available", C.BASS_ERROR_NOTAVAIL: "requested data is not available", C.BASS_ERROR_DECODE: "the channel is/isn't a 'decoding channel", C.BASS_ERROR_DX: "a sufficient DirectX version is not installed", C.BASS_ERROR_TIMEOUT: "connection timedout", C.BASS_ERROR_FILEFORM: "unsupported file format", C.BASS_ERROR_SPEAKER: "unavailable speaker", C.BASS_ERROR_VERSION: "invalid BASS version (used by add-ons)", C.BASS_ERROR_CODEC: "codec is not available/supported", C.BASS_ERROR_ENDED: "the channel/file has ended", C.BASS_ERROR_BUSY: "the device is busy", C.BASS_ERROR_UNSTREAMABLE: "BASS_ERROR_UNSTREAMABLE", C.BASS_ERROR_UNKNOWN: "some other mystery problem"}
+	codes=map[int]string {C.BASS_OK: "all is OK", C.BASS_ERROR_MEM:  "memory error", C.BASS_ERROR_FILEOPEN	: "can't open the file", C.BASS_ERROR_DRIVER: "can't find a free/valid driver", C.BASS_ERROR_BUFLOST: "the sample buffer was lost", C.BASS_ERROR_HANDLE: "invalid handle", BASS_ERROR_FORMAT: "unsupported sample format", C.BASS_ERROR_POSITION: "invalid position", C.BASS_ERROR_INIT: "BASS_Init has not been successfully called", C.BASS_ERROR_START: "BASS_Start has not been successfully called", C.BASS_ERROR_SSL: "SSL/HTTPS support isn't available", C.BASS_ERROR_ALREADY: "already initialized/paused/whatever", C.BASS_ERROR_NOTAUDIO: "NOTAUDIO", C.BASS_ERROR_NOCHAN: "can't get a free channel", C.BASS_ERROR_ILLTYPE: "an illegal type was specified", C.BASS_ERROR_ILLPARAM: "an illegal parameter was specified", C.BASS_ERROR_NO3D: "no 3D support", C.BASS_ERROR_NOEAX: "no EAX support", C.BASS_ERROR_DEVICE: "illegal device number", C.BASS_ERROR_NOPLAY: "not playing", C.BASS_ERROR_FREQ: "illegal sample rate", C.BASS_ERROR_NOTFILE: "the stream is not a file stream", C.BASS_ERROR_NOHW: "no hardware voices available", C.BASS_ERROR_EMPTY: "the MOD music has no sequence data", C.BASS_ERROR_NONET: "no internet connection could be opened", C.BASS_ERROR_CREATE: "couldn't create the file", C.BASS_ERROR_NOFX: "effects are not available", C.BASS_ERROR_NOTAVAIL: "requested data is not available", C.BASS_ERROR_DECODE: "the channel is/isn't a 'decoding channel", C.BASS_ERROR_DX: "a sufficient DirectX version is not installed", C.BASS_ERROR_TIMEOUT: "connection timedout", C.BASS_ERROR_FILEFORM: "unsupported file format", C.BASS_ERROR_SPEAKER: "unavailable speaker", C.BASS_ERROR_VERSION: "invalid BASS version (used by add-ons)", C.BASS_ERROR_CODEC: "codec is not available/supported", C.BASS_ERROR_ENDED: "the channel/file has ended", C.BASS_ERROR_BUSY: "the device is busy", C.BASS_ERROR_UNSTREAMABLE: "BASS_ERROR_UNSTREAMABLE", C.BASS_ERROR_UNKNOWN: "some other mystery problem"}
 
 )
 /*
@@ -95,9 +98,9 @@ func SetVol(v float32) (bool, error) {
 // StreamCreateURL
 // HSTREAM BASSDEF(BASS_StreamCreateURL)(const char *url, DWORD offset, DWORD flags, DOWNLOADPROC *proc, void *user);
 func StreamCreateURL(url string) (Channel, error) {
-	ch := C.BASS_StreamCreateURL(C.CString(url), 0, C.BASS_STREAM_BLOCK|C.BASS_STREAM_STATUS|C.BASS_STREAM_AUTOFREE, nil, nil)
+	ch:= Channel(C.BASS_StreamCreateURL(C.CString(url), 0, C.BASS_STREAM_BLOCK|C.BASS_STREAM_STATUS|C.BASS_STREAM_AUTOFREE, nil, nil))
 	if ch != 0 {
-		return int(ch), nil
+		return ch, nil
 	} else {
 		return 0, errMsg(int(C.BASS_ErrorGetCode()))
 	}
@@ -105,10 +108,10 @@ func StreamCreateURL(url string) (Channel, error) {
 
 // BASS_StreamCreateFile
 // HSTREAM BASSDEF(BASS_StreamCreateFile)(BOOL mem, const void *file, QWORD offset, QWORD length, DWORD flags);
-func StreamCreateFile(file string) (Channel, error) {
-	ch := C.BASS_StreamCreateFile(0, unsafe.Pointer(C.CString(file)), 0, 0, C.BASS_ASYNCFILE|C.BASS_STREAM_AUTOFREE)
+func StreamCreateFile(file string, flags int) (Channel, error) {
+	ch := Channel(C.BASS_StreamCreateFile(0, unsafe.Pointer(C.CString(file)), 0, 0, C.ulong(flags)))
 	if ch != 0 {
-		return int(ch), nil
+		return ch, nil
 	} else {
 		return 0, errMsg(int(C.BASS_ErrorGetCode()))
 	}
@@ -116,8 +119,8 @@ func StreamCreateFile(file string) (Channel, error) {
 
 // ChannelPlay
 // BOOL BASSDEF(BASS_ChannelPlay)(DWORD handle, BOOL restart);
-func (self Channel) ChannelPlay() (bool, error) {
-	if C.BASS_ChannelPlay(self, 1) != 0 {
+func (self Channel) Play() (bool, error) {
+	if C.BASS_ChannelPlay(C.DWORD(self), 1) != 0 {
 		return true, nil
 	} else {
 		return false, errMsg(int(C.BASS_ErrorGetCode()))
@@ -126,8 +129,8 @@ func (self Channel) ChannelPlay() (bool, error) {
 
 // ChannelPause
 // BOOL BASSDEF(BASS_ChannelPause)(DWORD handle);
-func (self Channel) ChannelPause() (bool, error) {
-	if C.BASS_ChannelPause(self) != 0 {
+func (self Channel) Pause() (bool, error) {
+	if C.BASS_ChannelPause(C.DWORD(self)) != 0 {
 		return true, nil
 	} else {
 		return false, errMsg(int(C.BASS_ErrorGetCode()))
@@ -136,8 +139,8 @@ func (self Channel) ChannelPause() (bool, error) {
 
 // ChannelStop
 // BOOL BASSDEF(BASS_ChannelStop)(DWORD handle);
-func (self Channel) ChannelStop() (bool, error) {
-	if C.BASS_ChannelStop(self) != 0 {
+func (self Channel) Stop() (bool, error) {
+	if C.BASS_ChannelStop(C.DWORD(self)) != 0 {
 		return true, nil
 	} else {
 		return false, errMsg(int(C.BASS_ErrorGetCode()))
@@ -146,30 +149,16 @@ func (self Channel) ChannelStop() (bool, error) {
 
 // ChannelStatus
 // DWORD BASSDEF(BASS_ChannelIsActive)(DWORD handle);
-func (self Channel) ChannelStatus() (c uint, s string) {
-	c = uint(C.BASS_ChannelIsActive(self))
-	switch c {
-	case 0:
-		s = "BASS_ACTIVE_STOPPED"
-		break
-	case 1:
-		s = "BASS_ACTIVE_PLAYING"
-		break
-	case 2:
-		s = "BASS_ACTIVE_STALLED"
-		break
-	case 3:
-		s = "BASS_ACTIVE_PAUSED"
-		break
-	}
-	return c, s
+func (self Channel) Status() uint {
+	val:=uint(C.BASS_ChannelIsActive(self.cint()))
+	return val
 }
 
 // ChannelGetAttribute
 // BOOL BASSDEF(BASS_ChannelGetAttribute)(DWORD handle, DWORD attrib, float *value);
-func (self Channel) ChannelGetAttribute(attrib int) (float32, error) {
+func (self Channel) GetAttribute(attrib int) (float32, error) {
 	var cvalue C.float
-	if C.BASS_ChannelGetAttribute(self, C.DWORD(attrib), &cvalue) != 0 {
+	if C.BASS_ChannelGetAttribute(self.cint(), C.DWORD(attrib), &cvalue) != 0 {
 		if v, err := strconv.ParseFloat(fmt.Sprintf("%v", cvalue), 32); err != nil {
 			return -1, nil
 		} else {
@@ -182,8 +171,8 @@ func (self Channel) ChannelGetAttribute(attrib int) (float32, error) {
 
 // ChannelSetAttribute
 // BOOL BASSDEF(BASS_ChannelSetAttribute)(DWORD handle, DWORD attrib, float value);
-func (self Channel) ChannelSetAttribute(attrib int, value float32) (bool, error) {
-	if C.BASS_ChannelSetAttribute(self, C.DWORD(attrib), C.float(value)) != 0 {
+func (self Channel) SetAttribute(attrib int, value float32) (bool, error) {
+	if C.BASS_ChannelSetAttribute(self.cint(), C.DWORD(attrib), C.float(value)) != 0 {
 		return true, nil
 	} else {
 		return false, errMsg(int(C.BASS_ErrorGetCode()))
@@ -192,34 +181,18 @@ func (self Channel) ChannelSetAttribute(attrib int, value float32) (bool, error)
 
 //ChannelGetLevel
 //DWORD BASSDEF(BASS_ChannelGetLevel)(DWORD handle);
-func (self Channel) ChannelGetLevel(ch int) (c int, e error) {
-	c = int(C.BASS_ChannelGetLevel(self)
+func (self Channel) GetLevel() (c int, e error) {
+	c = int(C.BASS_ChannelGetLevel(self.cint()))
 	if c == -1 {
 		return 0, errMsg(int(C.BASS_ErrorGetCode()))
 	}
 	return c, nil
 }
 
-// ChannelGetVolume
-// value: 0-100
-func ChannelGetVolume(ch int) (float32, error) {
-	v, err := ChannelGetAttribute(ch, BASS_ATTRIB_VOL)
-	if v > 0 {
-		v = v * 100
-	}
-	return v, err
-}
-
-// ChannelSetVolume
-// value: 0-100
-func ChannelSetVolume(ch int, value float32) (bool, error) {
-	return ChannelSetAttribute(ch, BASS_ATTRIB_VOL, value/100)
-}
-
 // ChannelGetTags
 // const char *BASSDEF(BASS_ChannelGetTags)(DWORD handle, DWORD tags);
-func ChannelGetTags(ch int, tag int) string {
-	return C.GoString(C.BASS_ChannelGetTags(C.DWORD(ch), C.DWORD(tag)))
+func (self Channel) GetTags(tag int) string {
+	return C.GoString(C.BASS_ChannelGetTags(self.cint(), C.DWORD(tag)))
 }
 
 // PluginLoad
