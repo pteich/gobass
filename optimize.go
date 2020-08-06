55995a55ba4bf7ebe9e25e39dd742169136c4bc8
keithcat1 <keithwipf1@gmail.com>
2020-08-05 20:31:31 -0500

    Actually added the optimized function, or more accuritly, its file. Oops!

---
+++ optimize.go
@@ -0,0 +1,21 @@
+//Contains optimized functions which can do multiple things in one CGO call.
+
+package bass
+/*
+#include "bass.h"
+static int _BassOptimizeSetPanVolumePitch(HCHANNEL chan, float pan, float volume, float pitch) {
+if(BASS_ChannelSetAttribute(chan, BASS_ATTRIB_PAN, pan)==0) { return BASS_ErrorGetCode(); }
+if(BASS_ChannelSetAttribute(chan, BASS_ATTRIB_VOL, volume)==0) { return BASS_ErrorGetCode(); }
+if(BASS_ChannelSetAttribute(chan, BASS_ATTRIB_FREQ, pitch)==0) { return BASS_ErrorGetCode(); }
+return 0;
+}
+*/
+import "C"
+
+func OptimizeSetPanVolumePitch(self Channel, pan, volume, pitch float32) error {
+	if C._BassOptimizeSetPanVolumePitch(self.cint(), C.float(pan), C.float(volume), C.float(pitch))==0 {
+		return nil
+	} else {
+		return errMsg()
+	}
+}
