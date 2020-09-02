cf261b35441ecb57f46bfa0b9cd8af3c9cbdbdfb
keithcat1 <keithwipf1@gmail.com>
2020-09-02 16:36:11 -0500

    More bugfixes

---
+++ bass_nonwindows.go
@@ -0,0 +1,27 @@
+// +build !windows
+
+package bass
+
+/*
+
+#include "bass.h"
+#include "stdlib.h"
+*/
+import "C"
+
+
+import (
+	"unsafe"
+)
+func Init(device int, freq int, flags int, hwnd, clsid uintptr) (bool, error) {
+	window:=unsafe.Pointer(hwnd)
+	gid:=unsafe.Pointer(clsid)
+	if C.BASS_Init(C.int(device), C.DWORD(freq), C.DWORD(flags), (*C.struct_HWND__)(window), (*C.struct__GUID)(gid)) != 0 {
+		return true, nil
+	} else {
+		return false, errMsg()
+	}
+}
+
+
+
