deb7cbb6501108882241321037f17dd28234ffe1
keithcat1 <keithwipf1@gmail.com>
2020-09-02 16:23:40 -0500

    Updated a file that was not in the previous commit where it should have been

--- bass_cgo_flags.go
+++ bass_cgo_flags.go
@@ -5,8 +5,7 @@ package bass
 #cgo linux,amd64 LDFLAGS: -lbass_linux_amd64
 #cgo windows,386 LDFLAGS: -lbass_windows_386
 #cgo windows,amd64 LDFLAGS: -lbass_windows_amd64
-#cgo darwin,386 LDFLAGS: -lbass_darwin_386
-#cgo darwin,amd64 LDFLAGS: -lbass_darwin_amd64
+#cgo darwin LDFLAGS: -lbass
 #include "bass.h"
 #include "stdlib.h"
 #cgo CFLAGS: -I${SRCDIR}/include
