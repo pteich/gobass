3061b8665d31e52fb71f40a3c4d0d7403f9c8f15
keithcat1 <keithwipf1@gmail.com>
2021-07-25 11:59:32 -0500

    Fix error handling for BASS_ChannelGetData and BASS_StreamPutData. When there is an error, these two functions return -1, instead of 0 like other BASS functions. Futher complicated by the fact that it returns an unsigned integer and -1 overflows

--- bass.go
+++ bass.go
@@ -373,11 +373,21 @@ func (self Channel) StreamPutData(data []byte) (int64, error) {
 	if len(data)==0 {
 		return 0, nil
 	}
-	return longPairToError(C.BASS_StreamPutData(self.cint(), unsafe.Pointer(&data[0]), cuint(len(data))))
+	val := C.BASS_StreamPutData(self.cint(), unsafe.Pointer(&data[0]), C.DWORD(len(data)))
+	if val +1 == 0 { // -1 indicates an error, but this is an unsigned integer
+		return 0, errMsg()
+	} else {
+		return int64(val), nil
+	}
 }
 func (self Channel) GetData(data []byte, flags int) (int64, error) {
 	if len(data)==0 {
 		return 0, nil
 	}
-	return longPairToError(C.BASS_ChannelGetData(self.cint(), unsafe.Pointer(&data[0]), C.DWORD(len(data))))
+	val := C.BASS_ChannelGetData(self.cint(), unsafe.Pointer(&data[0]), C.DWORD(len(data)))
+	if val +1 == 0 { // -1 indicates an error, but this is an unsigned integer
+		return 0, errMsg()
+	} else {
+		return int64(val), nil
+	}
 }
