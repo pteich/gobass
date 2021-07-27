92991ce870d4c687985aafec216d8d1f1701844b
keithcat1 <keithwipf1@gmail.com>
2021-07-26 20:43:55 -0500

    Figured out how to get GCC to complain if we get a BASS callback definition wrong

--- callbacks.go
+++ callbacks.go
@@ -6,11 +6,12 @@ package bass
 STREAMPROC* _GO_STREAMPROC_DEVICE = STREAMPROC_DEVICE;
 STREAMPROC* _GO_STREAMPROC_PUSH = STREAMPROC_PUSH;
 STREAMPROC* _GO_STREAMPROC_DUMMY = STREAMPROC_DUMMY;
-BOOL _GoBassRecordCallbackStreamPutData(DWORD recorder, char* buffer, int length, void* userdata) {
-	DWORD stream = (DWORD)(userdata);
+CALLBACK BOOL _GoBassRecordCallbackStreamPutData(DWORD recorder, const void* buffer, DWORD length, void* userdata) {
+DWORD stream = (DWORD)(userdata);
 	BASS_StreamPutData(stream, buffer, length);
 	return 1;
 }
+RECORDPROC _GoBassRecordCallbackStreamPutData; // if our callback doesn't match BASS definition of RECORDPROC, this line will tell us by complaining. Loudly.
 */
 import "C"
 
@@ -19,5 +20,5 @@ var (
 	STREAMPROC_DEVICE = C._GO_STREAMPROC_DEVICE
 	STREAMPROC_PUSH = C._GO_STREAMPROC_PUSH
 	STREAMPROC_DUMMY = C._GO_STREAMPROC_DUMMY
-	RecordCallbackStreamPutData = C._GoBassRecordCallbackStreamPutData
+	RecordCallbackStreamPutData = (*C.RECORDPROC)(C._GoBassRecordCallbackStreamPutData)
 )
