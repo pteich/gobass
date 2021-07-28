35d951c1c3613bfcd4a5c891b46820b58b96ca32
keithcat1 <keithwipf1@gmail.com>
2021-07-28 10:59:36 -0500

    Move a bunch of stuff around and update the headers and libraries to make it easier to add support for BASS extensions

--- bass_cgo_flags.go
+++ bass_cgo_flags.go
@@ -1,13 +1,12 @@
 package bass
 /*
-#cgo LDFLAGS: -L${SRCDIR}/lib
-#cgo linux,386 LDFLAGS: -lbass_linux_386
-#cgo linux,amd64 LDFLAGS: -lbass_linux_amd64
-#cgo windows,386 LDFLAGS: -lbass_windows_386
-#cgo windows,amd64 LDFLAGS: -lbass_windows_amd64
-#cgo darwin LDFLAGS: -lbass
-#include "bass.h"
-#include "stdlib.h"
+#cgo linux,386 LDFLAGS: -L${SRCDIR}/lib/linux_386
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib/linux_amd64
+#cgo windows,386 LDFLAGS: -L${SRCDIR}/lib/windows_386
+#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/lib/windows_amd64
+#cgo darwin LDFLAGS: -L${SRCDIR}/lib/darwin
+#cgo LDFLAGS: -lbass
+
 #cgo CFLAGS: -I${SRCDIR}/include
 */
 import "C"
