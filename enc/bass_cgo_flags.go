77dc1fa17b55d95a9b1d9e3c1faffdd2fcdb13d3
keithcat1 <keithwipf1@gmail.com>
2021-07-28 20:38:03 -0500

    On Linux, we now set -Wl,-Rpath=${SRCDIR}/lib/linux_*, so you can just run your app on the build computer without having to get the needed DLLS.

--- enc/bass_cgo_flags.go
+++ enc/bass_cgo_flags.go
@@ -1,7 +1,7 @@
 package enc
 /*
-#cgo linux,386 LDFLAGS: -L${SRCDIR}/../lib/linux_386
-#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../lib/linux_amd64
+#cgo linux,386 LDFLAGS: -L${SRCDIR}/../lib/linux_386 -Wl,-Rpath=${SRCDIR}/../lib/linux_386
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../lib/linux_amd64 -Wl,-Rpath=${SRCDIR}/../lib/linux_amd64
 #cgo windows,386 LDFLAGS: -L${SRCDIR}/../lib/windows_386
 #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/../lib/windows_amd64
 #cgo darwin LDFLAGS: -L${SRCDIR}/../lib/darwin
