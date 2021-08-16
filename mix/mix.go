37c89593581e960cd7a712ce4ac882bfc9281647
keithcat1 <keithwipf1@gmail.com>
2021-08-16 09:18:48 -0500

    MixerStreamCreate -> StreamCreate

--- mix/mix.go
+++ mix/mix.go
@@ -9,7 +9,7 @@ import (
 */
 import "C"
 type Mixer bass.Channel
-func MixerStreamCreate(freq, chans, flags bass.Flags) (Mixer, error) {
+func StreamCreate(freq, chans int, flags bass.Flags) (Mixer, error) {
 	return Mixer(C.BASS_Mixer_StreamCreate(C.DWORD(freq), C.DWORD(chans), C.DWORD(flags))).toError()
 }
 func (self Mixer) AddChannel(channel bass.Channel, flags bass.Flags) error {
